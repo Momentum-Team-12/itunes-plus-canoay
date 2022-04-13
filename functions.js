a47df215cf4af703c941f943386b2d7877298b4e
Ambar Cano <ambarcano@gmail.com>
2022-04-13 12:01:15 -0400

    working search

--- functions.js
+++ functions.js
@@ -1,7 +1,26 @@
 // Gerardo's function: minimizes lines of code on main script page by automating create element, class setting and innerText/HTML actions into one function.
+
 function buildElement(elementType, className, text) {
   let element = document.createElement(elementType);
   element.classList.add(className);
   element.innerText = text;
   return element;
+}
+
+// function buildElement(elementType, className, text) {
+//   let element = document.createElement(elementType);
+//   element.classList.add(className);
+//   if ((elementType = "audio" || "img")) {
+//     return (element.src = text);
+//   } else {
+//     element.innerText = text;
+//   }
+//   return element;
+// }
+
+// Clear search box function
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
 }
--- main.js
+++ main.js
@@ -1,49 +1,46 @@
 console.log("linked");
 
-let defaultText = " ";
-let searchBox = document.getElementById("search");
-searchBox.value = defaultText;
-
 let profileDiv = document.querySelector("#profile");
+let searchForm = document.querySelector("#sForm");
+let searchField = document.querySelector("#input");
+let searchAction = document.querySelector("#search");
+console.log(searchAction);
+searchForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log(searchField.value);
 
-fetch(
-  "https://proxy-itunes-api.glitch.me/search?term=dillinger-escape-plan&media=music",
-  {
-    method: "GET",
-  }
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    for (let result of data.results) {
-      // Stephen's workaround for art quality issue with Itunes API, slices out the max resolutions stated in fetch and adds higher resolution wording to art URL.
-      let artDirectLink = result.artworkUrl100.toString();
-
-      let artConvertedLink = artDirectLink.slice(0, -13) + "300x300bb.jpg";
-
-      let cardDiv = buildElement("div", "card", " ");
-      profileDiv.appendChild(cardDiv);
-
-      let artDiv = buildElement("img", "art", " ");
-      artDiv.src = artConvertedLink;
-      cardDiv.appendChild(artDiv);
-
-      let songDiv = buildElement("div", "song", result.trackName);
-      // document.createElement("div");
-      // songDiv.classList.add("song");
-      // songDiv.innerText = result.trackName;
-      cardDiv.appendChild(songDiv);
-
-      let nameDiv = buildElement("div", "artist", result.artistName);
-      // document.createElement("div");
-      // nameDiv.classList.add("artist");
-      // nameDiv.innerText = result.artistName;
-      cardDiv.appendChild(nameDiv);
-
-      let dateDiv = buildElement("div", "date", "");
-      dateDiv.innerHTML = `Release Date: 
-      ${moment(result.releaseDate).format("MMM Do, YYYY")}`;
-      cardDiv.appendChild(dateDiv);
+  fetch(
+    `https://proxy-itunes-api.glitch.me/search?term=${searchField.value}&media=music`,
+    {
+      method: "GET",
     }
-  });
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for (let result of data.results) {
+        // Stephen's workaround for art quality issue with Itunes API, slices out the max resolutions stated in fetch and adds higher resolution wording to art URL.
+        let artDirectLink = result.artworkUrl100.toString();
+        let artConvertedLink = artDirectLink.slice(0, -13) + "300x300bb.jpg";
+
+        let cardDiv = buildElement("div", "card", " ");
+        profileDiv.appendChild(cardDiv);
+
+        let artDiv = buildElement("img", "art", " ");
+        artDiv.src = artConvertedLink;
+        cardDiv.appendChild(artDiv);
+
+        let songDiv = buildElement("div", "song", result.trackName);
+        cardDiv.appendChild(songDiv);
+
+        let nameDiv = buildElement("div", "artist", result.artistName);
+        cardDiv.appendChild(nameDiv);
+
+        let dateDiv = buildElement("div", "date", "");
+        dateDiv.innerHTML = `Release Date:
+      ${moment(result.releaseDate).format("MMM Do, YYYY")}`;
+        cardDiv.appendChild(dateDiv);
+      }
+    });
+});
